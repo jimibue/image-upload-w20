46dd30a5cdc5d7648d91828540a36d56c7e65f66
James Yeates <jimibue@gmail.com>
2021-01-15 10:29:36 -0700

    front end part

--- client/src/demo/Home.js
+++ client/src/demo/Home.js
@@ -1,8 +1,11 @@
-import {Header} from "semantic-ui-react"
+import { Header } from "semantic-ui-react";
+import DropZoneDemo from "../components/DropZoneDemo";
 
 export default () => {
   return (
-    <Header>Home</Header>
-  )
-
-}
+    <>
+      <Header>Home</Header>
+      <DropZoneDemo />
+    </>
+  );
+};
